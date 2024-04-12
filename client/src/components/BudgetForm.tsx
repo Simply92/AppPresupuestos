73c4e6fa45344b6c59dfa28e3eddaab8538237d3
Simply92 <pedrouner92@gmail.com>
2024-04-12 15:09:10 -0300

    listo

--- client/src/components/BudgetForm.tsx
+++ client/src/components/BudgetForm.tsx
@@ -3,44 +3,44 @@ import { useBudget } from "../hooks/useBudget"
 
 const BudgetForm = () => {
 
-    const [budget, setBudget] = useState(0)
-    const { dispatch } = useBudget()
+  const [budget, setBudget] = useState(0)
+  const { dispatch } = useBudget()
 
-    const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
-        setBudget(e.target.valueAsNumber)
-    }
-    const isValid = useMemo (() => {
-      return isNaN(budget) || budget <= 0
-    }, [budget])
-    
-    const hableSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault
-        dispatch({type: 'add-budget', payload: {budget}})
-    }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber)
+  }
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0
+  }, [budget])
+
+  const hableSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault
+    dispatch({ type: 'add-budget', payload: { budget } })
+  }
   return (
     <>
-    
-    <form className="space-y-5" onSubmit={hableSubmit}>
+
+      <form className="space-y-5" onSubmit={hableSubmit}>
         <div className="flex flex-col space-y-5">
-            <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
-                Definir Presupuesto
-            </label>
-            <input 
+          <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+            Definir Presupuesto
+          </label>
+          <input
             id="budget"
-            type="number" 
+            type="number"
             className="w-full bg-white border border-gray-200 p-2"
             placeholder="Define tu presupuesto"
             name="budget"
             min="0"
             value={budget}
-            onChange={handleChange}/>
+            onChange={handleChange} />
         </div>
         <input type="submit"
-        value='Definir presupuesto'
-        className="bg-blue-500 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-10"
-        disabled={isValid} />
-    </form>
-    
+          value='Definir presupuesto'
+          className="bg-blue-500 hover:bg-blue-600 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-10"
+          disabled={isValid} />
+      </form>
+
     </>
   )
 }
